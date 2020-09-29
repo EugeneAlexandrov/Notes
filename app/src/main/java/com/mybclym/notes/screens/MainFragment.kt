7e7ceffe561e97ed3dd99b389d78cf358813c423
Eugen <Alexandrov>
2020-09-29 23:45:18 +0300

    add navigation_graph and main fragments

---
+++ app/src/main/java/com/mybclym/notes/screens/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.mybclym.notes.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mybclym.notes.R
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/mybclym/notes/screens/StartFragment.kt
@@ -0,0 +1,15 @@
+package com.mybclym.notes.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mybclym.notes.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
